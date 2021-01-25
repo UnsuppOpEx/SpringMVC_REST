7e3a46b516b80d72a690f8ccaa623640549e45eb
Anton <zantn22@gmail.com>
2021-01-25 15:10:30 +0300

    create Global Exception Handler-a

--- src/main/java/com/antonzemskov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/antonzemskov/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,13 @@
 package com.antonzemskov.spring.rest.controller;
 
 import com.antonzemskov.spring.rest.entity.Employee;
+import com.antonzemskov.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.antonzemskov.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.antonzemskov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +27,11 @@ public class MyRESTController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in DB");
+        }
         return employee;
     }
 }
---
+++ src/main/java/com/antonzemskov/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,20 @@
+package com.antonzemskov.spring.rest.exception_handling;
+
+public class EmployeeIncorrectData {
+   private String info;
+
+    public EmployeeIncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public EmployeeIncorrectData(String info) {
+        this.info = info;
+    }
+}
